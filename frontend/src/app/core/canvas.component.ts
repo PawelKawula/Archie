ed5c44f5863f2635d34095d82ebf8ce1d769e4a0
Paweł Kawula <pawel.kawula@proton.me>
2026-02-08 14:42:00 +0100

    fixup! feat: added viewport with dragging and zooming

--- frontend/src/app/core/canvas.component.ts
+++ frontend/src/app/core/canvas.component.ts
@@ -17,8 +17,6 @@ export class Canvas implements OnInit {
     globalThis.__PIXI_APP__ = app;
 
     document.body.appendChild(app.canvas);
-    //
-    // app.canvas.style = 'position: fixed; right: 0; top: 0; z-index: -999';
 
     const viewport = new Viewport({
       screenWidth: window.innerWidth,
