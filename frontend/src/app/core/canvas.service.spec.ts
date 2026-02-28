df5ba2c208051b2fa44705298e1171b5b01604ed
Paweł Kawula <pawel.kawula@proton.me>
2026-02-28 20:47:28 +0100

    test: tests for canvas service
    tests for canvas service

--- frontend/src/app/core/canvas.service.spec.ts
+++ frontend/src/app/core/canvas.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { Canvas } from './canvas.service';
+import { ConfigurationError } from './exceptions';
 
 describe('Canvas', () => {
   let service: Canvas;
@@ -12,5 +13,17 @@ describe('Canvas', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should error if trying to use app before calling init', () => {
+    assert.throws(() => service.app, ConfigurationError);
+  });
+
+  it('should error if trying to use viewport before calling init', () => {
+    assert.throws(() => service.app, ConfigurationError);
+  });
+
+  it('should not error if trying to use viewport after calling init', () => {
+    assert.throws(() => service.app, ConfigurationError);
   });
 });
