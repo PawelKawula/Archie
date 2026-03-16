f246e64b8596cd8a724ac1c1dfacd1377b85dbf1
Paweł Kawula <pawel.kawula@proton.me>
2026-03-16 20:30:48 +0100

    feat: proper content of simulation dialog
    added proper controls for simulation in simulation popover

--- frontend/src/app/core/simulation-popover/simulation-popover.component.ts
+++ frontend/src/app/core/simulation-popover/simulation-popover.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmInputImports } from '@spartan-ng/helm/input';
 import { HlmLabelImports } from '@spartan-ng/helm/label';
 import { HlmPopoverImports } from '@spartan-ng/helm/popover';
+import { HlmSliderImports } from '@spartan-ng/helm/slider';
 
 @Component({
   selector: 'app-simulation-popover',
@@ -11,10 +12,13 @@ import { HlmPopoverImports } from '@spartan-ng/helm/popover';
     HlmButtonImports,
     HlmLabelImports,
     HlmInputImports,
+    HlmSliderImports,
   ],
   templateUrl: './simulation-popover.component.html',
   styleUrl: './simulation-popover.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'absolute' },
 })
-export class SimulationPopover {}
+export class SimulationPopover {
+  speed = signal([50]);
+}
